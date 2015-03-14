5a5600c83f81938d40a1e4ae7e0b36e67ebe05a6
bayram <bayram@Lenovo-PC>
2015-03-14 14:19:06 +0200

    son commit haa çözümü.UserIndex ekleme Signed-off-by:bayram <bayram@Lenovo-PC>

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 //git
 //oturum başlatma
 ob_start();
@@ -7,20 +8,21 @@ Session::init();
 error_reporting(0);
 //Türkçe Karekter kullanımı
 header('Content-Type: text/html; charset=utf-8');
+
 //System dosyalarımı include ediyoruz
 /*
  * bu function ile include etmediğimiz classları bu fonksiyonda belittiğimiz
  * adres dizinini de ayırıyor ve kendi include ediyor.
  * yani otomatik olarak bu adrestekileri include edecek kendisi.
  */
-function  __autoload($className){
-    include_once 'system/libs/'.$className.'.php';
-    
+function __autoload($className) {
+    include_once 'system/libs/' . $className . '.php';
 }
+
 //config dosyasını include ediyorum
 include_once 'app/config/config.php';
 
 
 //bootstarp classı bunu bölüp controller a yönlendirecek.
-$boot =new Bootstrap();
+$boot = new Bootstrap();
 ?>
